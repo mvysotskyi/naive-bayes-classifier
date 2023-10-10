fd78eaa5a30c1bb7d393b3d9c4b77432d392ea62
Mykola Vysotskyi <vysotskyi.pn@ucu.edu.ua>
2023-10-11 00:19:28 +0300

    code cleanup

--- classifier.R
+++ classifier.R
@@ -5,13 +5,23 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
-f1Score <- function(y_true, y_pred) {
+recall <- function(y_true, y_pred) {
     tp <- sum(y_true == y_pred & y_true == TRUE)
-    fp <- sum(y_true != y_pred & y_true == FALSE)
     fn <- sum(y_true != y_pred & y_true == TRUE)
 	
-    precision <- tp / (tp + fp)
-    recall <- tp / (tp + fn)
+    return (tp / (tp + fn))
+}
+
+precision <- function(y_true, y_pred) {
+    tp <- sum(y_true == y_pred & y_true == TRUE)
+    fp <- sum(y_true != y_pred & y_true == FALSE)
+	
+    return (tp / (tp + fp))
+}
+
+f1Score <- function(y_true, y_pred) {
+    recall <- recall(y_true, y_pred)
+    precision <- precision(y_true, y_pred)
 	
     return (2 * precision * recall / (precision + recall))
 }
@@ -23,16 +33,16 @@ accuracy <- function(y_true, y_pred)
 
 freqDataframe <- function(dataframe, column, stop_words = NULL)
 {
-	dataframe %>%
-		unnest_tokens(word, column) %>%
-		count(word, sort = TRUE) %>%
-		filter(!word %in% stop_words)
+    dataframe %>%
+	unnest_tokens(word, column) %>%
+	count(word, sort = TRUE) %>%
+	filter(!word %in% stop_words)
 }
 
 stop_words <- read_file("data/stop_words.txt")
-# https://stackoverflow.com/questions/27195912/why-does-strsplit-return-a-list
-splitted <- strsplit(stop_words, split='\n')
-splitted <- splitted[[1]]
+
+splittedStopWords <- strsplit(stop_words, split='\n')
+splittedStopWords <- splittedStopWords[[1]]
 
 
 naiveBayes <- setRefClass("naiveBayes",                      
@@ -42,7 +52,11 @@ naiveBayes <- setRefClass("naiveBayes",
 	hamFreq = "data.frame",
 	spamFreq = "data.frame",
 	hamCount = "numeric",
-	spamCount = "numeric"
+	spamCount = "numeric",
+
+	totalCount = "numeric",
+	hamClassProb = "numeric",
+	spamClassProb = "numeric"
     ),
     
     methods = list(
@@ -50,60 +64,45 @@ naiveBayes <- setRefClass("naiveBayes",
 	# messages and corresponding label for the message encoded as 0 or 1 
         # (binary classification task)
 	fit = function(X, y)
-	{
-	    print("Fitting model...")
-	    
-	    hamFreq <<- freqDataframe(X[X$Category=="ham", ], "Message", splitted)
-	    spamFreq <<- freqDataframe(X[X$Category=="spam", ], "Message", splitted)
+	{	    
+	    hamFreq <<- freqDataframe(X[X$Category=="ham", ], "Message", splittedStopWords)
+	    spamFreq <<- freqDataframe(X[X$Category=="spam", ], "Message", splittedStopWords)
 
 	    hamCount <<- sum(X$Category=="ham")
 	    spamCount <<- sum(X$Category=="spam")
-		
-	    print("Model fitted!")
+
+	    totalCount <<- hamCount + spamCount
+	    hamClassProb <<- hamCount / totalCount
+	    spamClassProb <<- spamCount / totalCount
 	},
                     
 	# return prediction for a single message 
 	predict = function(message)
 	{
-	    if(nchar(message) == 0) { 
-		print("Please provide a message to predict!")
+	    # Check if message is not empty
+	    if(nchar(message) == 0) { 	
 		return (NULL)
 	    }
+
 	    # Convert message to bag of words
 	    wrapperDf <- data.frame(word = message)
-	    tokens <- unnest_tokens(wrapperDf, word, word) %>% filter(!word %in% splitted)
-	    
-	    # Total messages count
-	    totalCount <- hamCount + spamCount
-	    hamClassProb <- hamCount / totalCount
-	    spamClassProb <- spamCount / totalCount
- 
+	    tokens <- unnest_tokens(wrapperDf, word, word) %>% filter(!word %in% splittedStopWords) 
 
-	    # Calculate probability of message being ham
 	    hamProb <- hamClassProb
-
-	    #print("tehre")
-	    for (i in 1:nrow(tokens))
-	    {
-    		word <- tokens[i, ]
-		
-		inHamCount <- 0
-		if(!is.na(any(hamFreq$word == word)) && any(hamFreq$word == word)) { inHamCount <- hamFreq[hamFreq$word == word, "n"]$n }
-
-		hamProb <- hamProb * (inHamCount + 1) / (hamCount + totalCount)
-	    }
-
-
-	    # Calculate probability of message being spam
 	    spamProb <- spamClassProb
 
 	    for (i in 1:nrow(tokens))
 	    {
-		word <- tokens[i, ]	
+    		word <- tokens[i, ]
 		
-		inSpamCount <- 0
-		if(!is.na(any(spamFreq$word == word)) && any(spamFreq$word == word)) { inSpamCount <- spamFreq[spamFreq$word == word, "n"]$n }
-
+		inHamCount <- ifelse(!is.na(any(hamFreq$word == word)) && any(hamFreq$word == word),
+				     hamFreq[hamFreq$word == word, "n"]$n,
+				     0)
+		inSpamCount <- ifelse(!is.na(any(spamFreq$word == word)) && any(spamFreq$word == word),
+				      spamFreq[spamFreq$word == word, "n"]$n,
+				      0)
+		
+		hamProb <- hamProb * (inHamCount + 1) / (hamCount + totalCount)
 		spamProb <- spamProb * (inSpamCount + 1) / (spamCount + totalCount)
     	    }
 
@@ -117,27 +116,33 @@ naiveBayes <- setRefClass("naiveBayes",
         # try how well your model generalizes to real world data! 
 	score = function(X_test, y_test)
 	{
-	    resultVector <- c()
-	    for(i in 1:nrow(X_test))
-	    {
-		message <- X_test[i, ]
-		prediction <- predict(message$Message)
-		
-		resultVector <- c(resultVector, prediction)
-	    }
+	    y_pred <- lapply(X_test$Message, function(message) { predict(message) })
+	    
+	    scores = c(
+		"accuracy"  = accuracy(y_test, y_pred),
+		"precision" = precision(y_test, y_pred),
+		"recall"    = recall(y_test, y_pred),
+		"f1Score"   = f1Score(y_test, y_pred)
+	    )
 
-	    return (f1Score(y_test, resultVector))
+	    return (scores)
 	}
 ))
 
 test <- read_csv("data/test.csv", show_col_types = FALSE)
 train <- read_csv("data/train.csv", show_col_types = FALSE)
 
+# Create and fit model
 model = naiveBayes()
-
 model$fit(train, train$Category)
+
+# Score model
 model$score(test, test$Category == "ham")
 
-model$predict("")
+# Test on single message
+model$predict("Hello, how are you?") # should return TRUE
+model$predict("WINNER!! This is the secret code to unlock the money: C3421.") # should return FALSE
 
+# Test on empty string
+model$predict("") # should return NULL
 
